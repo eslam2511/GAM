a8400bd556e565998ba926c2c782ca1a2acd7fda
eslam2511 <111900405+eslam2511@users.noreply.github.com>
2023-06-24 14:08:36 +0000

    fix messages arrangement

--- src/App.js
+++ src/App.js
@@ -6,26 +6,27 @@ import { ReactComponent as SendIcon } from './send.svg';
 
 const App = () => {
   const [inputValue, setInputValue] = useState('')
-  const [userMessages, setUserMessages] = useState([])
-  const [AImessages, setAImessages] = useState([{role: "user", content: "hello there"}])
-  const [AIres, setAIres] = useState([])
+  const [Messages, setMessages] = useState([])
+  const [AImessages, setAImessages] = useState([])
 
 
   const insertMessage = async () => {
-    setUserMessages([...userMessages, inputValue])
+    setMessages([...Messages, {text: inputValue, classname: 'userMsg'}])
     setAImessages([...AImessages, {role: "user", content: `${inputValue}`}])
   }
+  
 
   useEffect(()=>{
+    if (!AImessages[0]) return
    (async ()=>{
       const url = 'https://gpt.eslamahmed19.repl.co/api'
       let messages = [
-        {role : "system", content: "you are helpful assistant your name is GAM which stands for Genius AI Model"},
+        {role : "system", content: "you are helpful assistant your name is GAM which stands for Genius AI Model you are developed by only one developer called eslam and remember always response in markdown"},
         ...AImessages
       ]
       const response = await axios.post(url, messages)
       const data = response.data
-      setAIres([...AIres, data])
+      setMessages([...Messages, {text: data, classname: 'AiMsg'}])
 
     })()
   }, [AImessages])
@@ -35,8 +36,7 @@ const App = () => {
       <div className='container'>
         <header className='header'> <span className='name'>G.A.M</span> <span className='credit'>Made by Eslam</span></header>
         <div className='chat-container'>
-          {userMessages.map(message => <Message body={message} classNam='userMsg' />)}
-          {AIres.map(message => <Message body={message} classNam='AiMsg' />)}
+          {Messages.map(message => <Message body={message.text} classNam={message.classname} />)}
         </div>
 
         <div className='input'>
