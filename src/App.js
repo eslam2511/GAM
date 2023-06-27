fe059d3d1b34c09dd201e44734414d39aefb4642
eslam2511 <islamahmed008@gmail.com>
2023-06-27 13:56:13 +0300

    button statte variable

--- src/App.js
+++ src/App.js
@@ -8,35 +8,48 @@ const App = () => {
   const [inputValue, setInputValue] = useState('')
   const [Messages, setMessages] = useState([])
   const [AImessages, setAImessages] = useState([])
+  const [sendbtn, setSendbtn] = useState(null);
 
 
-  const insertMessage = async () => {
-    setMessages([...Messages, {text: inputValue, classname: 'userMsg'}])
-    setAImessages([...AImessages, {role: "user", content: `${inputValue}`}])
+  const insertMessage = () => {
+    if (inputValue === '') return
+    setMessages(prevMessages => [...prevMessages, { text: inputValue, classname: 'userMsg' }]);
+    setAImessages(prevState => [...prevState, { role: "user", content: `${inputValue}` }])
   }
   
+  useEffect(() => {
+    const button = (
+      <button className='send' onClick={insertMessage}>
+        <SendIcon className='sendIcon' />
+      </button>
+    );
+    setSendbtn(button); // Update sendbtn whenever inputValue changes
+  }, [inputValue]);
 
-  useEffect(()=>{
-    if (!AImessages[0]) return
-   (async ()=>{
+  useEffect(() => {
+    if (!AImessages[0]) return;
+    (async () => {
       const url = 'https://gpt.eslamahmed19.repl.co/api'
       let messages = [
-        {role : "system", content: "you are helpful assistant your name is GAM which stands for Genius AI Model you are developed by only one developer called eslam"},
+        { role: "system", content: "you are helpful assistant your name is GAM which stands for Genius AI Model you are developed by only one developer called eslam" },
         ...AImessages
       ]
       const response = await axios.post(url, messages)
       const data = response.data
-      setMessages([...Messages, {text: data, classname: 'AiMsg'}])
-
+  
+      if (data) {
+        setMessages(prevState => [...prevState, { text: data, classname: 'AiMsg' }])
+      }
     })()
   }, [AImessages])
+  
 
   return (
     <>
       <div className='container'>
         <header className='header'> <span className='name'>G.A.M</span> <span className='credit'>Made by Eslam</span></header>
         <div className='chat-container'>
-          {Messages.map(message => <Message body={message.text} classNam={message.classname} />)}
+          {Messages.map((message, index) => <Message key={index} body={message.text} classNam={message.classname} />)}
         </div>
 
         <div className='input'>
@@ -44,16 +57,13 @@ const App = () => {
             className='input-field'
             placeholder='hello there...'
             value={inputValue}
-            onChange={e => setInputValue(e.target.value)}
+            onChange={e => {setInputValue(e.target.value)
+              console.log(inputValue);
+            }}
 
           />
 
-          <button
-            className='send'
-            onClick={insertMessage}
-          >
-            <SendIcon className='sendIcon' />
-          </button>
+          {sendbtn}
 
         </div>
 
