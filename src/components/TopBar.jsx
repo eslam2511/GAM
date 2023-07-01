06383cc0d9f9463937465d4c76de7a6f1b6ac26c
eslam2511 <islamahmed008@gmail.com>
2023-07-01 20:46:07 +0300

    top bar added

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import '../style/TopBar.css';
+
+const TopBar = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        setIsHidden(false); // Scrolling upwards
+      } else {
+        setIsHidden(true); // Scrolling downwards
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
+  return (
+    <div className={`top-bar ${isHidden ? 'hidden' : ''}`}>
+      <header className='header'> <span className='name'>G.A.M</span> <span className='credit'>Made by Eslam</span></header>
+    </div>
+  );
+};
+
+export default TopBar;
